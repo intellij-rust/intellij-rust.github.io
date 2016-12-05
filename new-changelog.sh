166165eb38a620344161a8a7d7a12de203444ca7
Aleksey Kladov <aleksey.kladov@gmail.com>
2016-12-05 16:14:00 +0300

    Quick and dirty new changelog script
    I know I should be using Jekyll compose or something instead.

---
+++ new-changelog.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+next_post=$(($(ls _posts | wc -l) - 1))
+today=$(date +"%Y-%m-%d")
+
+cat << EOF > _posts/$today-changelog-$next_post.markdown
+---
+layout: post
+title: "IntelliJ Rust Changelog #$next_post"
+date: $(date +"%Y-%m-%d %H:%M:%S %z")
+---
+
+## New Features
+
+## Fixes
+
+## Refactorings
+
+[@w]: https://github.com/w
+EOF
